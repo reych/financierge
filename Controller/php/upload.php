6015ce2822843775dcce3a50d0d8e9cb75f5c0c5
Zhongyang Gao <billgao0807@gmail.com>
2016-03-27 16:33:02 -0700

    organized folders
    added model view controller folder

--- Controller/php/upload.php
+++ Controller/php/upload.php
@@ -1,5 +1,5 @@
 <?php
-include 'vendor/autoload.php';
+include '../../vendor/autoload.php';
 use Parse\ParseClient;
 use Parse\ParseObject;
 use Parse\ParseQuery;
@@ -74,5 +74,5 @@ else{
 }
 //delete file from temporary directory to avoid conflicts with future uploads
 unlink($target_file);
-header("Location:index.html");
+// header("Location:index.html");
 ?>
