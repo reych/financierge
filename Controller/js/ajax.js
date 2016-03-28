7b6744d751bc8c178fec8aca70f177e269eb506c
Zhongyang Gao <billgao0807@gmail.com>
2016-03-27 17:28:30 -0700

    ajax structure specified

--- Controller/js/ajax.js
+++ Controller/js/ajax.js
@@ -1,10 +1,10 @@
-function ajaxTest(funcName) {
+function phpRequest(funcName) {
     $.ajax({
         type:'post',
         url: '../Controller/php/ajaxTest.php',
         data: {funcToCall: funcName},
-        success: function(dtx){
-            alert(dtx);
+        success: function(phpResponse){
+            alert(phpResponse);
         }
     });
 }
