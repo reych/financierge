fce869992b3073c021118a98b3f8a5f3e1b0f3ef
iamchristianv <iamchristianv@gmail.com>
2016-03-28 02:10:01 -0700

    updated API for Network class

--- network.php
+++ network.php
@@ -1,17 +1,44 @@
 <?php
-session_start();
-include 'vendor/autoload.php';
+/*
+
+-----------------
+NETWORK CLASS API
+-----------------
+
+Network::signupUser(username, password) -> true if signup was successful, false if otherwise
+
+Network::loginUser(username, password) -> user object if login was successful, NULL if otherwise
+
+Network::getCurrentUser() -> user object if user is logged in, NULL is otherwise
+
+Network::logoutUser() -> nothing
+
+Network::addAccount(name, isAsset) -> true if account was added sucessfully, false if otherwise
+
+Network::deleteAccount(id) -> true if account was deleted sucessfully, false if otherwise
+
+Network::addTransactionToAccount(date, principle, amount, category, id) -> true if transaction was added sucessfully, false if otherwise
+
+Network::getTransactionsForAccount(id) -> array of transactions if fetch was successful, NULL if otherwise
+
+Network::getTransactionsForAccountWithinDates(startDate, endDate, id) -> array of transactions if fetch was successful, NULL if otherwise
+
+*/
+
+include("vendor/autoload.php");
 use Parse\ParseClient;
 use Parse\ParseObject;
 use Parse\ParseQuery;
 use Parse\ParseException;
 use Parse\ParseUser;
-date_default_timezone_set('America/Los_Angeles');
-ParseClient::initialize('9DwkUswTSJOLVi7dkRJxDQNbwHSDlQx3NTdXz5B0', '6HFMDcw8aRr9O7TJ3Pw8YOWbecrdiMuAPEL3OXia', 'IdmvCVEBYygkFTRmxOwUvSxtnXwlaGDF9ndq5URq');
+
+session_start();
+date_default_timezone_set("America/Los_Angeles");
+ParseClient::initialize("9DwkUswTSJOLVi7dkRJxDQNbwHSDlQx3NTdXz5B0", "6HFMDcw8aRr9O7TJ3Pw8YOWbecrdiMuAPEL3OXia", "IdmvCVEBYygkFTRmxOwUvSxtnXwlaGDF9ndq5URq");
 
 class Network {
 
-	function signupUser($username, $password) {
+	static function signupUser($username, $password) {
 		$user = new ParseUser();
 		$user->set("username", $username);
 		$user->set("password", $password);
@@ -23,7 +50,7 @@ class Network {
 		}
 	}
 
-	function loginUser($username, $password) {
+	static function loginUser($username, $password) {
 		try {
 			$user = ParseUser::logIn($username, $password);
 			return $user;
@@ -32,32 +59,33 @@ class Network {
 		}
 	}
 
-	function currentUser() {
+	static function getCurrentUser() {
 		return ParseUser::getCurrentUser();
 	}
 
-	function logoutUser() {
+	static function logoutUser() {
 		ParseUser::logOut();
 	}
 
-	function addAccount($name, $isAsset) {
+	static function addAccount($name, $isAsset) {
 		// return true or false
 	}
 
-	function deleteAccount($name) {
+	static function deleteAccount($id) {
 		// return true or false
 	}
 
-	function addTransactionToAccount($date, $principle, $amount, $category, $account) {
+	static function addTransactionToAccount($date, $principle, $amount, $category, $id) {
 		// return true or false 
 	}
 
-	function getTransactionsForAccount($account) {
+	static function getTransactionsForAccount($id) {
 		// return array
 	}
 
-	function getTransactionsForAccountWithinDates($startDate, $endDate, $account) {
+	static function getTransactionsForAccountWithinDates($startDate, $endDate, $id) {
 		// return array (inclusive)
 	}
 }
+
 ?>
