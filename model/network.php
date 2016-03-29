c4f38e6d9de00890d3d9a8074d8c860a1a6031c9
iamchristianv <iamchristianv@gmail.com>
2016-03-28 21:34:22 -0700

    implemented more transaction methods in Network class

--- model/network.php
+++ model/network.php
@@ -27,9 +27,9 @@ Network::getTransactionsForAccountWithinDates(startDate, endDate, id) -> array o
 
 include("vendor/autoload.php");
 use Parse\ParseClient;
+use Parse\ParseException;
 use Parse\ParseObject;
 use Parse\ParseQuery;
-use Parse\ParseException;
 use Parse\ParseUser;
 
 session_start();
@@ -71,12 +71,10 @@ class Network {
 
 	static function addAccount($name, $isAsset) {
 		try {
-			// create account
 			$account = new ParseObject("Account");
 			$account->set("name", $name);
 			$account->set("isAsset", $isAsset);
 			$account->save();
-			// add account to current user accounts
 			$currentUser = ParseUser::getCurrentUser();
 			$accounts = $currentUser->get("accounts");
 			$accounts[] = $account->getObjectId(); // try to save the object itself
@@ -89,11 +87,10 @@ class Network {
 		}
 	}
 
-	static function deleteAccount($id) {
+	static function deleteAccount($accountID) {
 		try {
-			// fetch account
 			$accountQuery = new ParseQuery("Account");
-			$account = $accountQuery->get($id);
+			$account = $accountQuery->get($accountID);
 			$account->destroy();
 			return true;
 		} catch (ParseException $error) {
@@ -102,17 +99,52 @@ class Network {
 		}
 	}
 
-	static function addTransactionToAccount($date, $principle, $amount, $category, $id) {
-
-		// return true or false 
+	static function addTransactionToAccount($date, $principle, $amount, $category, $accountID) {
+		try {
+			$transaction = new ParseObject("Transaction");
+			$transaction->set("date", $date);
+			$transaction->set("principle", $principle);
+			$transaction->set("amount", $amount);
+			$transaction->set("category", $category);
+			$transaction->save();
+			$accountQuery = new ParseQuery("Account");
+			$account = $accountQuery->get($accountID);
+			$transactions = $account->get("transactions");
+			$transactions[] = $transaction->getObjectId();
+			$account->setArray("transactions", $transactions);
+			$account->save();
+			return true;
+		} catch (ParseException $error) {
+			echo $error->getMessage();
+			return false;
+		}
 	}
 
-	static function getTransactionsForAccount($id) {
-		// return array
+	static function getTransactionsForAccount($accountID) {
+		try {
+			$accountQuery = new ParseQuery("Account");
+			$account = $accountQuery->get($id);
+			$transactionIDs = $account->get("transactions");
+			$transactions = [];
+			foreach ($transactionIDs as $transactionID) {
+				$transactionQuery = new ParseQuery("Transaction");
+				$transaction = $transactionQuery->get($transactionID);
+				$transactions[] = $transaction;
+			}
+			return $transactions;
+		} catch (ParseException $error) {
+			echo $error->getMessage();
+			return NULL;
+		}
 	}
 
-	static function getTransactionsForAccountWithinDates($startDate, $endDate, $id) {
-		// return array (inclusive)
+	static function getTransactionsForAccountWithinDates($startDate, $endDate, $accountID) {
+		try {
+		
+		} catch (ParseException $error) {
+			echo $error->getMessage();
+			return NULL;
+		}
 	}
 }
 ?>
