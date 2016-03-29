694caa3c022984f01ba6dbfdb542bd266bf00fc0
Rena Chen <renachen@usc.edu>
2016-03-29 15:37:11 -0700

    transactions controller

--- Controller/js/transactionsController.js
+++ Controller/js/transactionsController.js
@@ -1,1 +1,61 @@
 /* Write transactions code here */
+
+/* Display the list of transactions in the container with containerID
+ * transaction_list: date_category_amount_principle
+ */
+function displayTransactions(containerID, transactions_list) {
+    //split data
+    var transactionsArray = transactions_list.split('\n');
+    //create table
+    var table = document.createElement('TABLE');
+    createTableHeaders(table, 'Date_Category_Amount_Principle');
+
+    //insert transaction data into table
+    for(i=0; i<transactionsArray.length; i++) {
+        var transactionDataArray = transactionsArray[i].split('_');
+        var tableRow = document.createElement('TR');
+
+        for(j=0; j<transactionDataArray.length; j++) {
+            var tableData = document.createElement('TD');
+            var value = document.createTextNode(transactionDataArray[j]);
+            tableData.appendChild(value);
+            tableRow.appendChild(tableData);
+        }
+
+        table.appendChild(tableRow);
+    }
+
+    //add table to container
+    var container = document.getElementById(containerID);
+    container.appendChild(table);
+
+}
+
+/* Clear the container of its children */
+function clearChildren(container) {
+    while (container.firstChild) {
+      	container.removeChild(container.firstChild);
+  	}
+}
+
+/* Create headers for a table */
+function createTableHeaders(table, headers_string){
+	//create a new row
+	var tableRow = document.createElement("TR");
+	var tHead = document.createElement("THEAD");
+    //Split headers_string into array
+    var tableHeaders = headers_string.split('_')
+	//create table header elements and content
+	for(i = 0; i < tableHeaders.length; i++){
+		var ticker = document.createElement("TH");
+		var content = document.createTextNode(tableHeaders[i]);
+		ticker.appendChild(content);
+		var alignAttr = document.createAttribute("align");
+		alignAttr.value = "left";
+		ticker.setAttributeNode(alignAttr);
+		tableRow.appendChild(ticker);
+	}
+	//add row to watchlist content
+	tHead.appendChild(tableRow);
+	table.appendChild(tHead);
+}
