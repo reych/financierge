afb0d7b8bb1b4f28aed2968e3c261419c193cef2
Zhongyang Gao <billgao0807@gmail.com>
2016-03-30 22:24:28 -0700

    refactoring: restructured ajax calls to make codes more readable
    added close button for transaction tabs
    added load account list function

---
+++ Controller/js/showAccounts.js
@@ -0,0 +1,24 @@
+function displayAccounts(account_list) {
+    var container = document.getElementById('account-list');
+    clearChildren(container);
+    //split data
+    var accountsArray = account_list.split('\n');
+    //create table
+    var table = document.createElement('TABLE');
+    createTableHeaders(table, 'Accounts_Balance');
+
+    //insert transaction data into table
+    for(i=0; i<accountsArray.length; i++) {
+        var tableRow = document.createElement('TR');
+        var tableData = document.createElement('TD');
+        var value = document.createTextNode(accountsArray[j]);
+
+        tableData.appendChild(value);
+        tableRow.appendChild(tableData);
+        table.appendChild(tableRow);
+    }
+
+    //add table to container
+    container.appendChild(table);
+
+}
