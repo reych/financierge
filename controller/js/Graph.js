9fa35be933eaeb2d3a95a19bd2068075a38dba55
vaudrinm <vaudrinm@usc.edu>
2016-04-11 21:44:57 -0700

    graph is ready to accept values via addOrUpdateAccount() in Graph.js

--- controller/js/Graph.js
+++ controller/js/Graph.js
@@ -2,46 +2,6 @@
 var namesToDataArrays = new Map();
 var dataSets = [];
 
-generateChartData();
-
-
-function generateChartData() {
-
-    var values = "assets|2016-04-10_10|2016-04-11_11|2016-04-12_12|2016-04-13_13|2016-04-14_14|2016-04-15_15\nliabilities|2016-04-10_15|2016-04-11_16|2016-04-12_17|2016-04-13_18|2016-04-14_19|2016-04-15_20\nnetworth|2016-04-10_2|2016-04-11_3|2016-04-12_4|2016-04-13_5|2016-04-14_6|2016-04-15_7";
-    var lines = values.split('\n');
-
-
-    for (i = 0; i < lines.length; i++) {
-        var nodes = lines[i].split('|');
-        var name = nodes[0];
-        var dataArr = [];
-
-        for(j = 1; j < nodes.length; j++) {
-
-            var dateValues = nodes[j].split('_');
-            var dateArr = dateValues[0].split('-');
-
-            var newDate = new Date(parseInt(dateArr[0]), parseInt(dateArr[1]), parseInt(dateArr[2]));
-
-            var newValue = parseFloat(dateValues[1]);
-
-            dataArr.push({
-                    "date" : newDate,
-                    "value" : newValue
-            });
-        }
-
-        dataSets.push({
-                  "title": name,
-          "fieldMappings": [{"fromField": "value", "toField": "value"}],
-           "dataProvider": dataArr,
-          "categoryField": "date"
-
-            });
-        namesToDataArrays.set(name, dataArr);        
-    }
-}
-
 var chart = AmCharts.makeChart( "chartdiv", {
   "type": "stock",
   "theme": "light",
@@ -98,35 +58,112 @@ var chart = AmCharts.makeChart( "chartdiv", {
   "dataSetSelector": {
     "position": "left"
   },
+  "panelsSettings":{"recalculateToPercents" : "never"},
 
   "export": {
     "enabled": true
   }
 } );
 
+generateChartData();
+
+
+function generateChartData() {
+
+    var values = "assets|2015-10-10_10|2016-01-11_11|2016-03-12_12|2016-04-13_13|2016-04-14_2\nliabilities|2016-04-10_15|2016-04-11_16|2016-04-12_17|2016-04-13_12|2016-04-14_19|2016-04-15_20\nnetworth|2016-04-10_2|2016-04-11_3|2016-04-12_4|2016-04-13_5|2016-04-14_6|2016-04-15_7";
+    addOrUpdateAccount(values);
+
+    /*
+    var lines = values.split('\n');
+
+    // for each line split from the string, which represents an account and its transactions
+    for (i = 0; i < lines.length; i++) {
+        var nodes = lines[i].split('|');
+        var name = nodes[0];
+        var dataArr = [];
+
+        for(j = 1; j < nodes.length; j++) {
+
+            var dateValues = nodes[j].split('_');
+            var dateArr = dateValues[0].split('-');
+
+            var newDate = new Date(parseInt(dateArr[0]), parseInt(dateArr[1]), parseInt(dateArr[2]));
+
+            var newValue = parseFloat(dateValues[1]);
+
+            dataArr.push({
+                    "date" : newDate,
+                    "value" : newValue
+            });
+        }
+
+        this.dataSets.push({
+                  "title": name,
+          "fieldMappings": [{"fromField": "value", "toField": "value"}],
+           "dataProvider": dataArr,
+          "categoryField": "date"
+
+            });
+        this.namesToDataArrays.set(name, dataArr);        
+    }
+    */
+}
+
+
+
+function addOrUpdateAccount(accountData) {
+  //var values = "assets|2015-10-10_10|2016-01-11_11|2016-03-12_12|2016-04-13_13|2016-04-14_2\nliabilities|2016-04-10_15|2016-04-11_16|2016-04-12_17|2016-04-13_12|2016-04-14_19|2016-04-15_20\nnetworth|2016-04-10_2|2016-04-11_3|2016-04-12_4|2016-04-13_5|2016-04-14_6|2016-04-15_7";
+    var lines = accountData.split('\n');
+
+    // for each line split from the string, which represents an account and its transactions
+    for (i = 0; i < lines.length; i++) {
+        var nodes = lines[i].split('|');
+        var name = nodes[0];
+        var dataArr = [];
+
+        for(j = 1; j < nodes.length; j++) {
+
+            var dateValues = nodes[j].split('_');
+            var dateArr = dateValues[0].split('-');
+
+            var newDate = new Date(parseInt(dateArr[0]), parseInt(dateArr[1]), parseInt(dateArr[2]));
+
+            var newValue = parseFloat(dateValues[1]);
+
+            dataArr.push({
+                    "date" : newDate,
+                    "value" : newValue
+            });
+        }
+
+        
+        this.namesToDataArrays.set(name, dataArr);        
+    }
+    updateDataSets();
+
+}
+
+function updateDataSets() {
+    this.dataSets = [];
+    for (var [name, dataArray] of this.namesToDataArrays) {
+        this.dataSets.push({
+                  "title": name,
+          "fieldMappings": [{"fromField": "value", "toField": "value"}],
+           "dataProvider": dataArray,
+          "categoryField": "date"
+
+            });
+    }
+
+    this.chart.dataSets = this.dataSets;
+    this.chart.validateNow();
+    this.chart.validateData();
+    
+}
+
 function testThisChart() {
 
-   //  var graph = new AmCharts.AmGraph();
-   //  graph.valueField = "savings";
-   //  graph.type = "line";
-   //  graph.id = "newOne";
-   // // chart.removeGraph(graph);
-   //  chart.addGraph(graph);
-    chart.dataSets = [];
-    chart.dataSets = [{
-        "date": "1990-12-02",
-        "savings": 0.255
-    }, {
-        "date": "1991-12-02",
-        "savings": 0.21
-    }, {
-        "date": "1992-12-02",
-        "savings": 0.065
-    }, {
-        "date": "1993-12-02",
-        "savings": 0.11
-    }];
-
-    chart.validateNow();
-    chart.validateData();
+   var values = "Jeffs Playtime Fun Money Bags|2015-10-10_-10|2016-01-11_-11|2016-03-12_-12|2016-04-13_13|2016-04-14_-2";
+   addOrUpdateAccount(values);
+    
 }
