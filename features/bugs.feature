eaefc60bcb0c6bec97270f2bb7b053332540d2aa
liangchr <liangchr@usc.edu>
2016-04-27 21:01:04 -0700

    cry

---
+++ features/bugs.feature
@@ -0,0 +1,33 @@
+Feature: Tests for Bug
+
+	Scenario: When user tries to invoke budget information without specifying a category, amount spent and budget values should be assigned to zero
+		Given user is logged in
+		And user clears category field
+		And user selects month of may 
+		When user clicks “go”
+		Then the budget and amount values should be set to zero
+
+	Scenario: When user tries to invoke budget information without specifying a date, amount spent and budget values should be assigned to zero
+		Given user is logged in
+		And user enters “food” into category field
+		And user clears month in input field
+		When user clicks “go”
+		Then the budget and amount values should be set to zero
+
+	Scenario: When user tries to invoke graph interval without specifying a end date, graph doesn’t do anything
+		Given user is logged in
+		And user enters start date “4/14/16”
+		And user clears end date input field
+		When user clicks “go”
+		Then the graph does nothing
+
+	Scenario: When user tries to reset budget, budget resets
+		Given user is logged in
+		And user enters “food” into category field
+		And user selects month of may 
+		And user has input the information by clicking “go”
+		And user enters five dollars in budget input field
+		And user clicks “change budget” button
+		And user enters ten dollars in budget input field
+		When user clicks “change budget” button
+		Then the budget for food should be set to five dollars without using upload
--- features/step_definitions/budget_steps.rb
+++ features/step_definitions/budget_steps.rb
@@ -3,61 +3,63 @@ require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 
 Given(/^user enters “food” into category field$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
+	currentURL = driver.current_url
 end
 
 Given(/^user selects month of may$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
+	currentURL = driver.current_url
 end
 
 Given(/^user has input the information by clicking “go”$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Given(/^user enters five dollars in budget input field$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 When(/^user clicks “change budget” buttonThen the budget for food should be set to five dollars$/) do
-
+	currentURL = driver.current_url
 end
 
 Given(/^ten dollars have been spent on food$/) do
-
+	currentURL = driver.current_url
 end
 
 When(/^user clicks “change budget” button$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Then(/^the amount spent value should be displayed in red on the dashboard$/) do
-  driver.find_element(:id, "budgety").click
+	currentURL = driver.current_url
 end
 
 Given(/^user enters ten dollars in budget input field$/) do
-
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Then(/^the amount spent value should be displayed in yellow on the dashboard$/) do
-  driver.find_element(:id, "budgety").click
-  driver.find_element(:id, "budgety").click
+
 end
 
 Given(/^user enters fifteen dollars in budget input field$/) do
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Then(/^the amount spent value should be displayed in green on the dashboard$/) do
-  driver.find_element(:id, "budgety").click
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Then(/^the budget details for the month of may should be displayed$/) do
-  driver.find_element(:id, "budgety").click
+	driver.manage.timeouts.implicit_wait = 1
 end
 
 Then(/^the user should see budget and amount spent values on the dashboard$/) do
-  driver.find_element(:id, "budgety").click
+	currentURL = driver.current_url
 end
 
 Then(/^the budget for food should be set to five dollars without using upload$/) do
-  driver.find_element(:id, "budgety").click
+	currentURL = driver.current_url
 end
